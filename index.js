67743433f42345db0c33584a17ef77865335d303
Alexandra Mejías Herrera <alexandramh1001@gmail.com>
2024-02-29 11:54:45 +0000

    sequelize

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 require('dotenv').config() //lo ponemos arriba del todo siempre
 
+const morgan = require('morgan')
 const express = require('express') //lanzar servidor
+const sequelize = require('./db')
 
 const api = express()  //express arrancado dentro de api
+
+
+const dbCheck = async () => {
+    try {
+        await sequelize.authenticate()
+        console.log('Connected to Guaguas DB')
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+
+api.use(morgan('dev'))
+api.use(express.json())
+api.get('/', (req, res) => res.send('Connected to Guaguas'))
 
 api.listen(process.env.PORT, (error) => { //servidor en el puerto 3000
     if (error) throw new Error('Cannot start API')
