3124bc3f390322372c271ef6fd1424042222595c
Alexandra Mejías Herrera <alexandramh1001@gmail.com>
2024-02-29 14:45:57 +0000

    login

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -12,14 +12,41 @@ const signup = async (req, res) => {
         const token = jwt.sign( //crear el token
             { email: user.email }, //identifica al usuario como unico
             process.env.JWT_SECRET, //secret
-            { expiresIn: '7d' }
+            { expiresIn: '7d' } //solo se pone al terminar la aplicacion
         )
         //res.status(200).send('Account created')
-        res.status(200).json( {token} )
+        //res.status(200).json( { token }) //lo podemos poner sin llaves y te devuelve el token solo, asi te puestra el objeto con su clave valor
+        res.status(200).json( {token: token, message: "Account created"} ) //json devuelve un objeto lo ponemos asi para poder mostrar dos cosas 
     } catch (error) {
         console.log(error)
         res.status(500).send('Error signing up')
     }
 }
 
-module.exports = signup
+const login = async (req, res) => {
+    try {
+        const user = await User.findOne({
+            where: {
+                email: req.body.email
+            }
+        })
+
+        if (!user) return res.status(500).send('Email or password incorrect') 
+        if (!bcrypt.compareSync(req.body.password, user.password)) return res.status(500).send('Password incorrect')//contraseña sin encriptar que meten a logearse y contraseña encriptada creada cuando se signup
+        
+        const token = jwt.sign( //crear el token
+            { email: user.email }, //identifica al usuario como unico
+            process.env.JWT_SECRET, //secret
+            { expiresIn: '7d' } //solo se pone al terminar la aplicacion
+        )
+        res.status(200).json({ token: token }) 
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('Error logging up')
+    }
+}
+
+module.exports = {
+    signup,
+    login
+}
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 
-const  signup  = require('../controllers/auth')
+const  { signup, login }  = require('../controllers/auth')
+
 
 router.post('/signup', signup)
+router.post('/login', login)
 
 module.exports = router
